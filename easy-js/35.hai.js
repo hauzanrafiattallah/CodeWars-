d55ae93bb9980536197582056b310669edf3de57
hauzanrafiattallah <rafihauzan42@gmail.com>
2024-01-30 22:31:51 +0700

    35/366

--- easy-js/35.hai.js
+++ easy-js/35.hai.js
@@ -1,1 +1,11 @@
-// Halo
+// https://www.codewars.com/kata/5545f109004975ea66000086
+
+function isDivisible(n, x, y) {
+  if (n % x == 0 && n % y == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isDivisible(48, 3, 4));
